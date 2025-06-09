94b2a59e17878910f4d0a194c279bc413ff7869c
GediminasVaicaitis <gedasltu22@gmail.com>
2025-06-09 19:48:57 +0300

    added

---
+++ fastapi_todos.py
@@ -0,0 +1,37 @@
+# a fastapi system for Todos management
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import datetime
+
+app = FastAPI()
+
+class Todos(BaseModel):
+    id: int
+    name: str
+    description: str
+    date: datetime.date
+
+# type hint for a list of Todos
+todos:list[Todos] = []
+
+@app.post("/todos/")
+def create_todo(todo: Todos):
+    todos.append(todo)
+    return {"message": "Todo created successfully"}
+
+@app.get("/todos/")
+def get_todos():
+    return todos
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+    return {"message": "Such todo was not found"}
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    todos[:] = [todo for todo in todos if todo.id != todo_id]
+    return {"message": "Todo deleted successfully"}
